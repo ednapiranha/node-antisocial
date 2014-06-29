5a1f9f8d257e3e0d4e5e46c99a35b0af3a07daee
Jennifer Fong <jen@ednapiranha.com>
2014-06-29 15:28:19 -0700

    initial tests with ursa

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+var level = require('level');
+var ursa = require('ursa');
+var chat = require('level-threaded-chat');
+
+var Antisocial = function (privateKeySender) {
+  this.privateKey = privateKeySender;
+
+  this.encrypt = function (message, publicKey) {
+    publicKey = ursa.createPublicKey(this.privateKey.toPublicPem());
+
+    return publicKey.encrypt(new Buffer(message, 'utf8'));
+  };
+
+  this.decrypt = function (message, publicKey) {
+    var encoded = new Buffer(message, 'hex');
+
+    return this.privateKey.decrypt(encoded).toString();
+  };
+};
+
+module.exports = Antisocial;
--- test/test.index.js
+++ test/test.index.js
@@ -2,4 +2,32 @@
 
 process.env.NODE_ENV = 'test';
 
+var ursa = require('ursa');
 var should = require('should');
+var Antisocial = require('../index');
+var a = new Antisocial(ursa.generatePrivateKey());
+
+var privateKeyReceiver = ursa.generatePrivateKey().toPublicPem();
+var publicKeyReceiver = ursa.createPublicKey(privateKeyReceiver, 'utf8').toPublicPem();
+
+var privateKeySender = ursa.generatePrivateKey().toPublicPem();
+var publicKeySender = ursa.createPublicKey(privateKeySender, 'utf8').toPublicPem();
+
+describe('validate', function () {
+  it('should encrypt and decrypt a message to self', function () {
+    var encrypted = a.encrypt('This is a test for the sender', publicKeyReceiver);
+    console.log('encrypted message. ', encrypted.toString());
+    var decrypted = a.decrypt(encrypted, publicKeyReceiver);
+    console.log('decrypted message. ', decrypted);
+    decrypted.should.equal('This is a test for the sender');
+  });
+
+  it('should encrypt and decrypt a message to other', function () {
+    var encrypted = a.encrypt('This is a test for the receiver', publicKeySender);
+    console.log('encrypted message. ', encrypted.toString());
+    var decrypted = a.decrypt(encrypted, publicKeySender);
+    console.log('decrypted message. ', decrypted);
+    decrypted.should.equal('This is a test for the receiver');
+  });
+
+});
